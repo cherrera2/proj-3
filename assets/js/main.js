22b02c20c7e582ebe3a8996523f8095c9d4d7f65
cherrera2 <cherrera2@hawk.iit.edu>
2021-12-06 16:01:48 -0600

    Add audio files of different quality to music folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,6 @@ var size = 748877; // 748877 bytes or 748.9 kB
 var start, end;
 var getImg = new Image();
 
-document.getElementById("footer").style.color = "blue";
-
 window.addEventListener('load', (event) => {
   end = new Date().getTime();
   calculateBandwidth();
@@ -21,4 +19,48 @@ function calculateBandwidth() {
     var bandwidthSpeed = (toBits / accessTime / 1024 / 1024).toFixed(2);
     console.log("Your bandwidth is: " + bandwidthSpeed);
     // (1024/1024) Convert bps to Mbps
+
+    selectFiles(bandwidthSpeed);
+}
+
+function selectFiles(speed) {
+  var t1 = new Audio(document.getElementById('c-t1'));
+  var t2 = new Audio(document.getElementById('c-t2'));
+  var t3 = new Audio(document.getElementById('c-t3'));
+  var t4 = new Audio(document.getElementById('c-t4'));
+
+  if(speed > 5) {
+    t1.src = "assets/music/Keep_it_Moving-320.mp3"
+    t2.src = "assets/music/Wisdom_and_Happiness-320.mp3"
+    t3.src = "assets/music/In_My_Lifetime-320.mp3"
+    t4.src = "assets/music/In_My_Lifetime-320.mp3"
+
+    console.log(t4.src);
+
+    console.log("Selected music at: 320Kbps");
+  }
+
+  else if (speed <= 5 && speed >= 1.5) {
+    t1.src = "assets/music/Keep_it_Moving-192.mp3"
+    t2.src = "assets/music/Wisdom_and_Happiness-192.mp3"
+    t3.src = "assets/music/In_My_Lifetime-192.mp3"
+    t4.src = "assets/music/Fearless-192.mp3"
+
+    console.log("Selected music at: 192Kbps");
+  }
+
+  else if (speed < 1.5) {
+    t1.src = "assets/music/Keep_it_Moving-128.mp3"
+    t2.src = "assets/music/Wisdom_and_Happiness-128.mp3"
+    t3.src = "assets/music/In_My_Lifetime-128.mp3"
+    t4.src = "assets/music/Fearless-128.mp3"
+
+    console.log("Selected music at: 128Kbps");
+  }
+
+  t1.load();
+  t2.load();
+  t3.load();
+  t4.load();
+
 }
